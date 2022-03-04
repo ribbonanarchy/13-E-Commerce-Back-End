8b8b5f7f8450d6b9622e741aba1f09d6a9033ca5
Robyn Arnecke <robynarnecke@gmail.com>
2022-03-04 16:42:35 -0600

    All tag routes functional

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -43,12 +43,37 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagUpdate = await Tag.update(
+      {tag_name: req.body.tag_name},
+      {where: {id: req.params.id}}
+    );
+    res.status(200).json(tagUpdate);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
